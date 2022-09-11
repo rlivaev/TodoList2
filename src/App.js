03428fd406d637ae35827208a01a9cabddc26881
Rustam <ruslivaev05@gmail.com>
2022-09-11 20:40:47 +0600

    add todo to the todoList

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import "./App.css";
-import {useState} from "React";
+import {useState} from "react";
+
+
+
 
 function App() {
   const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+  const addTodo = () =>{
+    if(todo !== ""){
+      setTodos([...todos, todo]);
+      setTodo ("");
+    } 
+  }
   return (
     <div className="App">
      <h1>React Todo App</h1>
      <div className="input-wrapper">
-      <input type="text" name="todo"placeholder="Create a new todo"/>
-      <button className="add-buttom">Add</button>
+      <input 
+       onChance={(e) => {setTodo(e.target.value)}}
+       type="text" 
+       name="todo"
+       placeholder="Create a new todo"
+       />
+      <button onClick={addTodo} className="add-buttom">Add</button>
      </div>
     </div>
   );
