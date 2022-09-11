63da772658e6f91e3951589ae791837ccad35d22
Rustam <ruslivaev05@gmail.com>
2022-09-11 19:50:39 +0600

    add input and button

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ function App() {
   return (
     <div className="App">
      <h1>React Todo App</h1>
-     
+     <div className="input-wrapper">
+      <input type="text" name="todo"placeholder="Create a new todo"/>
+      <button className="add-buttom">Add</button>
+     </div>
     </div>
   );
   }
